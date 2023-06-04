2f8af453a52b3f9827185626ab513de8365dab33
Assami Muzaki <muzakiassami@gmail.com>
2023-06-04 19:22:31 +0700

    proto form pengiriman

--- app/Views/pengiriman/form.php
+++ app/Views/pengiriman/form.php
@@ -2,11 +2,73 @@
 
 <!-- Disini Contentnya apa -->
 <?= $this->section("content") ?>
-  <form id="f1">
-    <a href="<?=base_url('user/tambah')?>">LINK KE USER</a>
-    <h2>Nama</h2>
-    <input type="text" name="" id="">
-    <h2>No Telp</h2>
-    <input type="text" name="" id="">
+<div class="form-pengiriman">
+  <h2>From Pengiriman Barang</h2>
+  <form action="">
+    <div class="form-pengiriman-data">
+      <h3>Pengirim</h3>
+      <div class="form-pengiriman-label">
+        <label for="">
+          Nama
+          <input type="text" name="" id="">
+        </label>
+        <label for="">
+          No.Telp
+          <input type="text" name="" id="">
+        </label>
+        <label for="">
+          Alamat
+          <input type="text" name="" id="">
+        </label>
+      </div>
+    </div>
+    <div class="form-pengiriman-data">
+      <h3>Penerima</h3>
+      <div class="form-pengiriman-label">
+        <label for="">
+          Nama
+          <input type="text" name="" id="">
+        </label>
+        <label for="">
+          No.Telp
+          <input type="text" name="" id="">
+        </label>
+        <label for="">
+          Alamat
+          <input type="text" name="" id="">
+        </label>
+      </div>
+    </div>
+    <div class="form-pengiriman-data">
+      <h3>Barang</h3>
+      <div class="form-pengiriman-label-barang">
+        <label for="">
+          Nama
+          <input type="text" name="" id="">
+        </label>
+        <label for="">
+          Berat
+          <input type="text" name="" id="">
+        </label>
+        <label for="">
+          Tanggal Masuk
+          <input type="date" name="" id="">
+        </label>
+        <label for="">
+          Tanggal Keluar
+          <input type="date" name="" id="">
+        </label>
+        <label for="">
+          Status
+          <select name="" id="">
+            <option value="">ON PROGRES</option>
+            <option value="">SUCCESS</option>
+            <option value="">FAILED</option>
+          </select>
+        </label>
+      </div>
+    </div>
+    <input type="submit" value="Simpan">
   </form>
+</div>
 <?= $this->endSection() ?>
